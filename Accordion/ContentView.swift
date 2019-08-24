2c773fb22879a9629cd2b9cb018d6eda3eb4d777
Sarah Reichelt <sarah@troz.net>
2019-08-24 11:11:02 +1000

    Initial Commit

---
+++ Accordion/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Accordion
+//
+//  Created by Sarah Reichelt on 24/08/2019.
+//  Copyright © 2019 TrozWare. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
